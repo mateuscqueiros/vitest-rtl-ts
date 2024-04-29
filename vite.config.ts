0bdc8ad0bbfec06f49a98ef7121f300436171074
Mateus Queirós <mateuscqueiros@gmail.com>
2024-04-29 00:13:29 -0300

    Fix dependencies and wrong types

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: './src/tests/setup.ts',
     css: true
   }
 })
